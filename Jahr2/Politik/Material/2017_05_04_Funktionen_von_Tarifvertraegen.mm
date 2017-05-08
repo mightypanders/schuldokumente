5e87d0f17849d63de8a959c9f01f7cfd44c44dbc
mightypanders <mighty.panders@gmail.com>
2017-05-08 08:44:29 +0200

    many things

---
+++ Jahr2/Politik/Material/2017_05_04_Funktionen_von_Tarifvertraegen.mm
@@ -0,0 +1,24 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1493899363516" ID="ID_1513617985" MODIFIED="1493899607676" TEXT="Funktionen von Tarifvertr&#xe4;gen">
+<node CREATED="1493901101120" HGAP="-18" ID="ID_243974602" MODIFIED="1493902070166" POSITION="right" TEXT="Ordnungsfunktion" VSHIFT="-32">
+<node CREATED="1493901241436" ID="ID_207614573" MODIFIED="1493901256901" TEXT="Tarifvertrag regelt die Arbeitsverh&#xe4;ltnisse"/>
+<node CREATED="1493901258961" ID="ID_405490873" MODIFIED="1493901269354" TEXT="Branchenintern"/>
+<node CREATED="1493901271143" ID="ID_614097200" MODIFIED="1493901291945" TEXT="Vertragspartner: AG+AN+GEW"/>
+<node CREATED="1493901535877" ID="ID_1121450147" MODIFIED="1493901685337" TEXT="n&#xfc;tzt Arbeitnehmer, Arbeitgeber"/>
+</node>
+<node CREATED="1493901113349" HGAP="-82" ID="ID_458977501" MODIFIED="1493902683008" POSITION="left" TEXT="Schutzfunktion" VSHIFT="-55">
+<node CREATED="1493902478466" ID="ID_140622673" MODIFIED="1493902491996" TEXT="Arbeitnehmer wird vor Arbeitgeber gesch&#xfc;tzt"/>
+<node CREATED="1493902493898" ID="ID_842214361" MODIFIED="1493902564275" TEXT="vor Willk&#xfc;r"/>
+<node CREATED="1493902649525" ID="ID_1590524490" MODIFIED="1493902667235" TEXT="Arbeitnehmer werden vor anderen AN gesch&#xfc;tzt"/>
+<node CREATED="1493902566240" ID="ID_56778872" MODIFIED="1493902641074" TEXT="Preiskampf wird verhindert"/>
+<node CREATED="1493902683454" ID="ID_1411824572" MODIFIED="1493902689855" TEXT="n&#xfc;tzt Arbeitnehmern"/>
+</node>
+<node CREATED="1493901121530" HGAP="-184" ID="ID_468352662" MODIFIED="1493902162218" POSITION="right" TEXT="Friedensfunktion" VSHIFT="38">
+<node CREATED="1493901745970" ID="ID_1816199217" MODIFIED="1493901957847" TEXT="Keine Streiks w&#xe4;hrend Laufzeit"/>
+<node CREATED="1493901959551" ID="ID_5255791" MODIFIED="1493901985980" TEXT="auch bis 4 Wochen danach"/>
+<node CREATED="1493902163145" ID="ID_1401771953" MODIFIED="1493902175542" TEXT="bei nicht K&#xfc;ndigung wird der Vetrag verl&#xe4;ngert"/>
+<node CREATED="1493902012038" ID="ID_1250032173" MODIFIED="1493902027093" TEXT="n&#xfc;tzt Arbeitgeber und Staat"/>
+</node>
+</node>
+</map>
